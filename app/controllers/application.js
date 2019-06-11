245416f8340e9faf87bb45bf853e54c011ce2879
Jordan Smith <bear.chunda@gmail.com>
2019-06-10 21:48:36 -0500

    master

---
+++ app/controllers/application.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  // init() {
+  //   this._super(...arguments);
+  //   var cursorOffset = {
+  //     left: -30,
+  //     top: -20,
+  //   };
+  //   document.getElementById('container').addEventListener(
+  //     'mousemove',
+  //     function(e) {
+  //       var $cursor = document.getElementById('cursor');
+  //       $cursor.style.left = e.pageX - cursorOffset.left + 'px';
+  //       $cursor.style.top = e.pageY - cursorOffset.top + 'px';
+  //     },
+  //     false,
+  //   );
+  // },
+});
